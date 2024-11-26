f9d0b6f03e788f9368c200c568d1c931645df471
yuan02-up <1123716087@qq.com>
2024-11-26 19:52:28 +0800

    11/26 v.4

--- prompt-sw.js
+++ prompt-sw.js
@@ -2168,6 +2168,6 @@ class NavigationRoute extends Route {
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") self.skipWaiting();
 });
-precacheAndRoute([{"revision":null,"url":"assets/index-dpy376jU.js"},{"revision":null,"url":"assets/index-TRtLBwIf.css"},{"revision":null,"url":"assets/workbox-window.prod.es5-B9K5rw8f.js"},{"revision":"46f15d2ee92c6595679e8c6a0ccbe3f9","url":"index.html"},{"revision":"7cc871d352c1b61e1fdc2ad441d0cfa4","url":"manifest.webmanifest"}]);
+precacheAndRoute([{"revision":null,"url":"assets/index-CWZ9sJ_H.js"},{"revision":null,"url":"assets/index-TRtLBwIf.css"},{"revision":null,"url":"assets/workbox-window.prod.es5-B9K5rw8f.js"},{"revision":"d1af5a18185418fe16669a94ea1617d6","url":"index.html"},{"revision":"7cc871d352c1b61e1fdc2ad441d0cfa4","url":"manifest.webmanifest"}]);
 cleanupOutdatedCaches();
 registerRoute(new NavigationRoute(createHandlerBoundToURL("index.html")));
